bab3e18ef946a9959e8ca128a225abc8bfd6267c
Rogelio Garcia <rogelio.garcia@west-mec.org>
2016-01-29 15:03:44 -0700

    more products

---
+++ cart.php
@@ -0,0 +1,46 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="styleSheet.css">
+    <script src="script.js"></script>
+    <meta charset="UTF-8">
+    <title>Buy stuff</title>
+</head>
+
+<body style="background-color: lightgray">
+
+<h1 id="storeName">Buy Stuff</h1>
+<ul>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <ul style="float:right;list-style-type:none;">
+        <li><a href="#about">About</a></li>
+        <li><a href="cart.php"><img src="http://www.graphicsbeam.com/wp-content/uploads/2012/04/Shopping-Cart-Icon-with-PSD-and-PNG-Format.png" style="height: 30px;width: 40px;"></a></li>
+    </ul>
+</ul>
+<table>
+    <tr>
+        <td><img src="http://techwelike.com/wp-content/uploads/2014/03/TCL-48-inch-1080P-LED-HDTV-Sams-Club-TechWeLike-Cruz-2.png" style="height: 100px;width: 150px;"><p name="item1">48 inch 1080P</p><button id="prod1" onclick="enter()">Add to cart?</button></td>
+        <td><img src="http://pngimg.com/upload/computer_pc_PNG7713.png" style=" height: 100px;width: 150px;"><p>hp desktop</p></td>
+        <td><img src="http://assets.razerzone.com/eeimages/products/752/razer-ouroboros-gallery-1.png" style="height:100px; width: 150px;"><p>Gaming mouse</p></td>
+        <td><img src="http://image.alienware.com/images/landings/desktops/alienware-x51-right-fx-lime-green-2x500x500.png" style="height: 100px; width: 150px;"><p>Alienware x51</p></td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Wii_U_Console_and_Gamepad.png" style=" height: 100px;width: 150px;"><p>Wii u</p></td>
+        <td><img src="http://icons.iconarchive.com/icons/firstline1/movie-mega-pack-2/512/Shrek-icon.png" style="height: 100px; width: 150px;"><p>Shrek</p></td>
+        <td><img src="http://pngimg.com/upload/tablet_PNG8593.png" style="height: 100px; width: 150px;"><p>Sony Tablet</p></td>
+        <td><img src="http://www.com-www.briteklight.com/upload/12775441697113.png" style="height: 100px; width: 150px;"><p>HDMI Cable</p></td>
+        <td><img src="http://www.perixx.com/en/products/kb/design/photo/px-1000.png" style="height: 100px; width: 150px;"><p>Gaming Keyboard</p></td>
+    </tr>
+</table>
+
+
+
+
+
+
+</div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" type="text/css" href="styleSheet.css">
+    <meta charset="UTF-8">
+    <title>Buy stuff</title>
+</head>
+
+<body style="background-color: lightgray">
+
+<h1 id="storeName">Buy Stuff</h1>
+<ul>
+    <li><a class="active" href="index.php">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <ul style="float:right;list-style-type:none;">
+        <li><a href="#about">About</a></li>
+        <li><a href="cart.php"><img src="http://www.graphicsbeam.com/wp-content/uploads/2012/04/Shopping-Cart-Icon-with-PSD-and-PNG-Format.png" style="height: 30px;width: 40px;"></a></li>
+    </ul>
+</ul>
+<h2 id="weeklyDeals">Weekly deals</h2>
+<div id="Deals">
+    <table border="1" bgcolor="">
+        <tr>
+            <td><img src="http://techwelike.com/wp-content/uploads/2014/03/TCL-48-inch-1080P-LED-HDTV-Sams-Club-TechWeLike-Cruz-2.png" style="height: 350px;width: 400px;"><p>48 inch 1080P LED</p></td>
+            <td><img src="http://pngimg.com/upload/computer_pc_PNG7713.png" style="width: 450px; height: 400px;"><p>hp desktop</p></td>
+            <td><img src="http://assets.razerzone.com/eeimages/products/752/razer-ouroboros-gallery-1.png" style="height:400px; width: 450px;"><p>Gaming mouse</p></td>
+        </tr>
+        <tr>
+            <td><img src="http://image.alienware.com/images/landings/desktops/alienware-x51-right-fx-lime-green-2x500x500.png" style="height: 400px; width: 450px;"><p>Alienware x51</p></td>
+            <td><img src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Wii_U_Console_and_Gamepad.png" style="width: 400px; height: 300px;"><p>Wii u</p></td>
+            <td><img src="http://icons.iconarchive.com/icons/firstline1/movie-mega-pack-2/512/Shrek-icon.png" style="height: 400px; width: 400px;"><p>Shrek</p></td>
+        </tr>
+        <tr>
+            <td><img src="http://pngimg.com/upload/tablet_PNG8593.png" style="height: 400px; width: 400px;"><p>Sony Tablet</p></td>
+            <td><img src="http://www.com-www.briteklight.com/upload/12775441697113.png" style="height: 400px; width: 400px;"><p>HDMI Cable</p></td>
+            <td><img src="http://www.perixx.com/en/products/kb/design/photo/px-1000.png" style="height: 300px; width: 400px;"><p>Gaming Keyboard</p></td>
+        </tr>
+
+
+
+
+
+</div>
+</div>
+</body>
+</html>
